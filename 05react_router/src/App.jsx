4a38cc8f8786522e81444373beafe5691ac34ba9
Karan <karancs.sat@gmail.com>
2024-12-20 12:29:00 +0530

    react router demo

--- 05react_router/src/App.jsx
+++ 05react_router/src/App.jsx
@@ -1,12 +1,14 @@
-
+import Header from "./components/header/Header.jsx";
+import {Outlet} from "react-router";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
+          <Header />
+          <Outlet />
+          <Footer />
       </>
   )
 }
--- 05react_router/src/main.jsx
+++ 05react_router/src/main.jsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router";
+import App from "./App.jsx";
+import Home from "./components/home/Home.jsx";
+import About from "./components/about/AboutUs.jsx";
+import Contact from "./components/contact/ContactUs.jsx";
+
+//way 1
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "",
+                element: <Home />,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "contact",
+                element: <Contact />,
+            },
+
+        ]
+    }
+])
+
+// way 2
+const router1 = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />} >
+            <Route path="" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+        </Route>
+    )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router1} > </RouterProvider>
   </StrictMode>,
 )
